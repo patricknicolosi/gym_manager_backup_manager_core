3e6462398424cbc075063a16c6351808afe42652
Patrick Nicolosi <patricknicolosi99@gmail.com>
2025-05-12 18:54:21 +0200

    Add core library and export necessary files; remove obsolete test file

---
+++ lib/core.dart
@@ -0,0 +1,4 @@
+library gym_manager_backup_manager_core;
+
+export 'repositories/backup_repository.dart';
+export 'models/backup_shift.dart';
--- test/gym_manager_backup_manager_core_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:gym_manager_backup_manager_core/gym_manager_backup_manager_core.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
